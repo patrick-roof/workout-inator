20aefff3cecf777f338a10b6e845d4829066513a
patrick <roosterruf@gmail.com>
2023-02-06 21:11:10 -0700

    random change

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+console.log('hello')
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
