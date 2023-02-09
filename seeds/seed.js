79ed7b1e2fee5703aa710e07bd3ae89a627eb840
patrick <roosterruf@gmail.com>
2023-02-08 20:41:03 -0700

    edited seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,18 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Workout } = require('../models');
 
 const userData = require('./userData.json');
+const workoutData = require('./workoutData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const workouts = await Workout.bulkCreate(workoutData, {
     individualHooks: true,
     returning: true,
   });
