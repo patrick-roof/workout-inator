613d8cecf0f93e91b3ddf30e2dea50499bc34f81
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-02-09 20:41:19 -0700

    merging develop branch into ecadena7 branch.

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.MYSQL_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306
     }
--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    secondName: {
+    lastName: {
       type: DataTypes.STRING,
       allowNull: false,
     },
