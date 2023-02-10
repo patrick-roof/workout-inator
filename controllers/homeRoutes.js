e44ef8c26f3e36d437a0e209997633caaf315027
patrick <roosterruf@gmail.com>
2023-02-09 17:25:43 -0700

    edits made to start getting routes to work

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('homepage')
+})
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+});
+
+router.get('/survey', (req, res) => {
+    
+})
+
+module.exports = router
---
+++ public/js/profile.js
@@ -0,0 +1,4 @@
+console.log('hello')
+//submit button
+//event handler for form submission
+//pass info to userRoutes.js
