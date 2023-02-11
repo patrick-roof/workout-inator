faa98661ad7c69b771f06325e408ac03144dcdf3
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-02-11 14:02:40 -0700

    updated the homeRoutes to include a login requirements. also made some slight changes to wher the modal.js and styling are held.

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,4 +1,5 @@
 console.log('hello')
 //submit button
 //event handler for form submission
+    //The submit button and event handlers for the form are in the modal.js file.
 //pass info to userRoutes.js
