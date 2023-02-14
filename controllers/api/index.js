792bfe388cedf8b80ea2cfc0ad2c14373abfaadb
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-02-13 19:13:28 -0700

    updating partial handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const workoutRoutes = require('./workoutRoutes');
+ const workoutRoutes = require('./workoutRoutes');
 // What will the workoutRoutes be used for? --Eli
 
 router.use('/users', userRoutes);
-// router.use('/workouts', workoutRoutes);
+router.use('/workouts', workoutRoutes);
 
 module.exports = router;
