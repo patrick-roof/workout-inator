cbe27bae749d0ce21e096a74c4ad1fc7d00ac1a4
Jose Orozco <josearozco15@gmail.com>
2023-02-15 21:02:17 -0700

    merge

--- public/js/login.js
+++ public/js/login.js
@@ -12,28 +12,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/workoutpage');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
-  
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/workoutpage');
+        document.location.replace("/workoutpage");
       } else {
         alert(response.statusText);
       }
@@ -44,8 +23,6 @@ const loginFormHandler = async (event) => {
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  
 
     
