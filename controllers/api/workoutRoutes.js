d10623a77120a6f640572d94cf9e50afbb6044a7
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-02-15 18:09:35 -0700

    removed base code - I'm not sure we need a workoutRoutes file?

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,18 +1,1 @@
-// GET one workoutv -- TO DO
-router.get('/painting/:id', async (req, res) => {
-    try {
-      const dbPaintingData = await Painting.findByPk(req.params.id);
-  
-      const painting = dbPaintingData.get({ plain: true });
-  
-      res.render('painting', {
-        painting,
-        // We are not incrementing the 'countVisit' session variable here
-        // but simply sending over the current 'countVisit' session variable to be rendered
-        countVisit: req.session.countVisit,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+// i don't think we need this route..
